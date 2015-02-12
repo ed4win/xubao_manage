a536ac98316d41c3ce4a99a92e4e7cc654a33b75
ed4win <catqd@163.com>
2015-02-12 13:13:23 +0800

    update

---
+++ dx/dx_main_ex.php
@@ -0,0 +1,110 @@
+<?php
+session_start();
+
+//检测是否登录，若没登录则转向登录界面
+if(!isset($_SESSION['userid'])){
+	//header("Location:login.html");
+	exit();
+}
+define('IN_SYS', TRUE);
+	
+	$page = isset($_POST['page']) ? intval($_POST['page']) : 1;
+	$rows = isset($_POST['rows']) ? intval($_POST['rows']) : 10;
+	//mysql_query("SET NAMES utf-8");
+	include ('../conn/conn.php');
+	//mysql_real_escape_string() 防止依赖注入,但是要考虑连接的当前字符集
+	$engineno = isset($_POST['engineno']) ? mysql_real_escape_string($_POST['engineno']) : '123abcefg456';
+	$licenseno = isset($_POST['licenseno']) ? mysql_real_escape_string($_POST['licenseno']) : '123abc456efg';
+	//echo "tb-2333",$_POST['engineno'],"</br>";
+	if($engineno == null && $licenseno == null){
+		exit();
+	}
+	$str_licen = strlen($licenseno);
+	$str_eng = strlen($engineno);
+	
+	$userid = $_SESSION['userid'];
+	$username = $_SESSION['username'];
+	$usercode = $_SESSION['usercode'];
+	$user_com = $_SESSION['comcode'];
+	$searchtime = date('Y-m-d H:i:s');
+	$keyword = '';
+
+	/*
+	$com_query = mysql_query("select comcode from user where userid=$userid limit 1");
+	$com = mysql_fetch_row($com_query);
+	//echo 'bug1',$com[0];
+	$user_com = $com[0];
+	*/
+	
+	$offset = ($page-1)*$rows;
+	
+	$result = array();
+	$where = '';
+	$tbflag ='and dmflag = "1" and  xbflag = "0" ' ;
+
+	/*
+	//判断查询关键字,优先保存车牌号,放弃
+	if($str_licen==0){
+		$keyword .=  $engineno;
+	}
+	else if($str_eng==0){
+		$keyword .= $licenseno;
+	}else//(strlen($licenseno) >0 &&　strlen($engineno)>0)
+	{
+		$keyword .= $licenseno;
+	}
+	*/
+
+	//$engineno_utf = iconv('gb2312', 'utf-8',$engineno);
+	//$licenseno_utf = iconv('gb2312', 'utf-8',$licenseno);
+    if($user_com <>'441800')
+   {
+	if($str_licen==0){
+		$where .=  "engineno = '$engineno' and     substr(comcode,1,6) in ('441810','441811','441812','441813','441814','441815','441884','441885')";
+	}
+	else if($str_eng==0){
+		$where .= " licenseno = '$licenseno'   and     substr(comcode,1,6) in ('441810','441811','441812','441813','441814','441815','441884','441885') ";
+	}else//(strlen($licenseno) >0 &&　strlen($engineno)>0)
+	{
+		$where .= "engineno = '$engineno' and licenseno = '$licenseno'   and     substr(comcode,1,6) in ('441810','441811','441812','441813','441814','441815','441884','441885') ";
+	}
+}else{
+
+    if($str_licen==0){
+		$where .=  "engineno = '$engineno'  ";
+	}
+	else if($str_eng==0){
+		$where .= " licenseno = '$licenseno'    ";
+	}else//(strlen($licenseno) >0 &&　strlen($engineno)>0)
+	{
+		$where .= "engineno = '$engineno' and licenseno = '$licenseno'    ";
+	}
+}
+	//echo 'BUGBANG!',$where,'STR:',$str_eng,'--STR2:',$str_licen;
+	  
+	//$rs = mysql_query("select count(1) from xubaomain where " . $where . $tbflag)
+	//or die("无效查询: " . mysql_error());
+	$rs = $dbh->query("select count(1) from xubaomain_ex where " . $where . $tbflag)
+	or die("无效查询: " . $rs->errorInfo());
+	//echo 'select ',$rs;
+	//$row = mysql_fetch_row($rs);
+	$row = $rs->fetch(PDO::FETCH_NUM);
+	if($row[0] >= 1)//仅记录有效查询,主要原因系统加载时时会会插入一条无效的记录.
+		{
+			$searchSQL = "INSERT INTO operatesearchhistory(usercode,username,usercomcode,engineno,licenseno,searchtime) VALUES ('$usercode','$username','$user_com','$engineno','$licenseno','$searchtime') ";
+			//mysql_query($searchSQL,$conn);
+			$dbh->exec($searchSQL);
+		}
+	$result["total"] = $row[0];
+	
+	//$rs = mysql_query("select * from xubaomain where " . $where . $tbflag . " limit $offset,$rows");
+	$rs = $dbh->query("select * from xubaomain_ex where " . $where . $tbflag . " limit $offset,$rows");
+	$items = array();
+	//while($row = mysql_fetch_object($rs)){
+	while($row = $rs->fetchobject()){
+		array_push($items, $row);
+	}
+$result["rows"] = $items;
+	$dbh=null;
+	echo json_encode($result);
+?>
---
+++ dx/update_dx_ex.php
@@ -0,0 +1,48 @@
+﻿<?php
+session_start();
+//$id = intval($_REQUEST['id']);
+$proposalno = $_REQUEST['proposalno'];
+$phoneflag = $_REQUEST['phoneflag'];
+$jinduflag = $_REQUEST['jinduflag'];
+$remark = $_REQUEST['remark'];
+//$carkind = $_REQUEST['carkind'];
+//echo $phoneflag;
+//$listprice = $_REQUEST['listprice'];
+//$unitcost = $_REQUEST['unitcost'];
+//$attr1 = $_REQUEST['attr1'];
+//$status = $_REQUEST['status'];
+
+
+$username = $_SESSION['username'];
+$usercode = $_SESSION['usercode'];
+$user_com = $_SESSION['comcode'];
+$updatetime = date('Y-m-d H:i:s');
+
+define('IN_SYS', TRUE);
+include ('../conn/conn.php');
+
+$sql = "UPDATE xubaomain_ex set phoneflag ='$phoneflag',jinduflag = '$jinduflag' ,remark = '$remark'  ,updatetime  = now()  where proposalno='$proposalno'";
+$updatesql = "INSERT INTO operateUpdatehistory(usercode,username,usercomcode,update_proposalno,updatetime) values ('$usercode','$username','$user_com','$proposalno','$updatetime')";
+//$updatesql = "INSERT INTO operateUpdatehistory(usercode)VALUES('1111')";
+//echo $updatesql;
+//@mysql_query($sql);
+$dbh->exec($sql);
+//mysql_query($updatesql,$conn);
+$dbh->exec($updatesql);
+$dbh = null;
+//if (!$check_query) { // add this check.
+//    die('Invalid query: ' . mysql_error());
+//}
+echo json_encode(array(
+	//'itemid' => $itemid,
+	//'productid' => $productid,
+	//'listprice' => $listprice,
+	//'unitcost' => $unitcost,
+	//'attr1' => $attr1,
+    //'status' => $status
+	//回写phoneflag 标志
+    'phoneflag' => $phoneflag,
+    'jinduflag' => $jinduflag,
+    'remark' => $remark
+));
+?>
